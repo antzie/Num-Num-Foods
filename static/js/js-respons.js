3b786b10710a51fda9b9948af5d4a435d58df46b
antzie <yangja@orcon.net.nz>
2019-04-02 16:46:24 +1300

    Fixed weird side-menu font changing

--- static/js/js-respons.js
+++ static/js/js-respons.js
@@ -2,15 +2,16 @@
 // Version: 13-01-2019
 
 // Move side list left by same amount as its width.
-    function toggleSideMenu() {
-      var listDiv = document.getElementById("menu-side");
-      var width = document.getElementById("menu-side").offsetWidth;
-      /*var widthWorkAround = 250; /*250 is the specified px width of the side menu. 
-      					The menu exapnds up to 250px. Thus if you shift it
-      					across by the width it is now, it expands into the new room*/
-      if (listDiv.style.marginLeft === "10px" || listDiv.style.marginLeft == '') {
-        listDiv.style.marginLeft = -width - 1 + "px";
-      } else {
-        listDiv.style.marginLeft = "10px";
-      }
-    }
+function toggleSideMenu() {
+  var listDiv = document.getElementById("menu-side");
+  var width = document.getElementById("menu-side").offsetWidth;
+  /*var widthWorkAround = 250; /*250 is the specified px width of the side menu. 
+  					The menu exapnds up to 250px. Thus if you shift it
+  					across by the width it is now, it expands into the new room*/
+  if (listDiv.style.marginLeft === "10px" || listDiv.style.marginLeft == '') {
+    listDiv.style.marginLeft = -width - 1 + "px";
+  } else {
+    listDiv.style.marginLeft = "10px";
+  }
+}
+
